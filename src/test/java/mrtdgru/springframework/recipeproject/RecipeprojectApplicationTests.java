40a66bac1c619138cdb7a9e3bd6b0589572f6f79
Mert Doğru <mertdogru90@gmail.com>
2020-10-03 16:19:33 +0300

    Initialize project

---
+++ src/test/java/mrtdgru/springframework/recipeproject/RecipeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package mrtdgru.springframework.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
